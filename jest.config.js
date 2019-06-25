a87f9718651d99faece8cbf45df2e9d2ae432d94
Joon Ho Cho <joonho1101@gmail.com>
2019-06-24 23:40:38 -0700

    fieldArrayGroup

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.test.json',
-      diagnostics: false
+      diagnostics: false,
     },
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
