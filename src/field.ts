b67b7b15af3b758520dd51cc90e16a57bae77843
Joon Ho Cho <joonho1101@gmail.com>
2020-04-13 09:16:07 -0700

    defaultIsEmpty tests empty array

--- src/field.ts
+++ src/field.ts
@@ -70,7 +70,10 @@ export interface IFieldConfig<TValue, TInputValue, TError = any> {
 
 export const defaultIsEqual = <T>(a: T, b: T): boolean => a === b;
 export const defaultIsEmpty = <T>(a: T): boolean =>
-  a == null || (a as any) === '';
+  a == null || // nil
+  (a as any) === '' || // empty string
+  a !== a || // NaN
+  (Array.isArray(a) && a.length === 0); // empty array
 export const defaultFormatInput = <T>(x: T): T => x;
 
 const nullState = {
